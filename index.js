4cf7241cfc84f3086eb36e5e7550074db17d0e46
HoangHongNgo <honghoangngo@gmail.com>
2023-08-02 15:19:42 +0700

    feat: deploy json server

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
+// server.js
 const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
-
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
-
-const PORT = 8000
-
-server.listen(PORT, () => {
-    console.log(`JSON Server is running on http://localhost:${PORT}`)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
 })
