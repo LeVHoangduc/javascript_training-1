27ca43d20beba5ecd209dfed559d1710b4d8aac1
HoangHongNgo <honghoangngo@gmail.com>
2023-08-02 15:26:27 +0700

    feat: deploy jsonserver

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// server.js
-const jsonServer = require('json-server')
+const jsonServer = require('json-server') // importing json-server library
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const localIpAddress = require('local-ip-address')
+const Logger = require('@ptkdev/logger')
+const port = process.env.PORT || 3001 // you can use any port number here; i chose to use 3001
+const logger = new Logger()
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
+
+server.listen(port, () => {
+    logger.info(
+        `JSON Server is running - http://localhost:${port} - http://${localIpAddress()}:${port}`,
+    )
 })
